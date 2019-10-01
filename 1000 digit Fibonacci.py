9d7625fd0fdb0baae61e7210dbf6d6830036dabc
Rishi Raj Singh <creator@pop-os.localdomain>
2019-10-01 13:25:20 +0530

    Modified code for FS

--- 1000 digit Fibonacci.py
+++ 1000 digit Fibonacci.py
@@ -1,42 +1,12 @@
-#!/user/bin/python
-'''
-n1 = 0
-n2 = 1
-n3 = 1
-flag = 1
-count = 1
-m = 0
-while flag < 1000:
-    n3 = n1 + n2
-    n1 = n2
-    n2 = n3
-    count += 1
-    m = str(n3)
-    if len(m) == 1000:
-        flag = 1000
-print count
-'''
+a = 0
+b = 1
+print(a)
+print(b)
+for i in range(998):
+    c = b + a
+    print(c)
+    a = b
+    b = c
+    
 
-d1 = 1
-d2 = 3
-d3 = 5
-d4 = 7
-r = 8
-f = 10
-f1 = 12
-f2 = 14
-for i in range(1, 1001):
-    d1 += r
-
-    for j in range(1, 1001):
-        d2 += f
-        f += 8
-        d3 += f1
-        f1 += 8
-        d4 += f2
-        f2 += 8
-
-    r += 8
-
-sum1 = d1 + d2 + d3 + d4
-print sum1
+    
